4f11cfc7548585f5b603b045e8e8797d4591dbcd
ricardocousolr <ricardo.couso@liferay.com>
2021-05-12 17:54:01 +0200

    LPS-130057 Placebo commit

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/request/SearchRequestImpl.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/request/SearchRequestImpl.java
@@ -117,6 +117,7 @@ public class SearchRequestImpl {
 
 		searchContext.setAttribute("filterExpired", Boolean.TRUE);
 		searchContext.setAttribute("paginationType", "more");
+		// placebo comment
 
 		return searchContext;
 	}
